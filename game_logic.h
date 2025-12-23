02cae6249f430164848998b2934a7cc8cbd49561
AntonioTorres23 <atorre6880@comcast.net>
2025-12-23 10:57:20 -0700

    Add project files.

---
+++ game_logic.h
@@ -0,0 +1,10 @@
+#ifndef GAME_LOGIC_HEADER // if game logic header doesn't exist
+#define GAME_LOGIC_HEADER // define game logic header
+
+#include <glad/glad.h> // include GLAD; a lib that allows for 
+
+
+
+
+#endif // !GAME_LOGIC_HEADER
+
